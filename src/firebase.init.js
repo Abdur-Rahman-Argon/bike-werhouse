909078529c65a83c2b1f05183334ec8dc687cd87
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-02 14:56:51 +0600

    firebase api key hide

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,25 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBHA2Y1CQcrhATS6_bCKQRUUU7SBdA25Gs",
-  authDomain: "db-assainment-11-bc594.firebaseapp.com",
-  projectId: "db-assainment-11-bc594",
-  storageBucket: "db-assainment-11-bc594.appspot.com",
-  messagingSenderId: "960387266785",
-  appId: "1:960387266785:web:b85722dd41455c693b9fa0",
-  measurementId: "G-09J0EWNKM4",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 
